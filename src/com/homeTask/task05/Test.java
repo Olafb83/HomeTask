3fa2f8373f7d0b036c19fc263a1eb570529b9d49
Olaf <obserrver@list.ru>
2014-11-07 18:52:38 +0300

    1

--- src/com/homeTask/task05/Test.java
+++ src/com/homeTask/task05/Test.java
@@ -4,16 +4,25 @@ package com.homeTask.task05;
  */
 public class Test {
     public static void main (String[] args){
-        Teacher mat = new Teacher("Duke Java", 34, "M", 50000, "Computer Science");
-        Student first = new Student("Lunne Brooke", 16, "F", "HS95129", 3.5);
-        CollegeStudent second = new CollegeStudent("Ima Frosh", 18, "F", "UCB123", 4.0, 1, "English");
+        Teacher mat = new Teacher();
+                //("Duke Java", 34, "M", 50000, "Computer Science");
+        Student first = new Student;
+                //("Lunne Brooke", 16, "F", "HS95129", 3.5);
+        CollegeStudent second = new CollegeStudent;
+                //("Ima Frosh", 18, "F", "UCB123", 4.0, 1, "English");
         Person[] people = new Person[4];
         people[0] = mat;
         people[1] = first;
         people[2] = second;
-        people[3] = new Person("Coach Bob", 27, "M");
+        people[3] = new Person;
+                //("Coach Bob", 27, "M");
+        mat.setName("Duke Java");
+        mat.setAge(34);
+        mat.setGender("M");
+        mat.setSalary(50000);
+        mat.setSubject("Computer Science");
         for (Person p : people){
-            System.out.println(p.getName() + " " + p.getAge());
+            System.out.println(p.getName() + " " + p.getAge() + " " + p.getGender());
         }
     }
 }
