419feceddc4f9f2473b08c3d993cf76bd4829ebd
Olaf <obserrver@list.ru>
2014-11-26 21:25:02 +0200

    home_push

--- src/com/homeTask/task08/SetClass.java
+++ src/com/homeTask/task08/SetClass.java
@@ -2,9 +2,7 @@ package com.homeTask.task08;
 
 import com.sourceit.hometask.collections.SetUtils;
 
-import java.util.Collection;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 /**
  * Created by Olaf on 25.11.2014.
@@ -12,11 +10,21 @@ import java.util.SortedSet;
 public class SetClass implements SetUtils{
     @Override
     public SortedSet<String> orderedSet(Collection<Integer> collection, Set<String> set) throws NullPointerException {
-        return null;
+        if (collection == null || set == null) throw new NullPointerException();
+        TreeSet<String> mySet = new TreeSet<>();
+        TreeSet<Integer> myIntSet = new TreeSet<>();
+        mySet.addAll(set);
+        myIntSet.addAll(collection);
+        return mySet;
     }
 
     @Override
     public Set<Integer> customOrderSet(Integer... integers) throws NullPointerException {
-        return null;
+        if (integers.length == 0) throw new NullPointerException();
+        HashSet<Integer> myHSet = new HashSet<>();
+        for (int i = 0; i < integers.length; i++){
+            myHSet.add(integers[i]);
+        }
+        return myHSet;
     }
 }
